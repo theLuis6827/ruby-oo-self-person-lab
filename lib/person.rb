e4c116736a05bc45a9d3102874793fdf1f48d28c
theLuis6827 <lozano_22_lalo@yahoo.com>
2021-02-25 10:22:29 -0600

    commit tree 5f38b1f13a90943678922d36b10e686c20e9124d

--- lib/person.rb
+++ lib/person.rb
@@ -3,18 +3,20 @@ require 'pry'
 class Person
 
     attr_reader :name, :happiness, :hygiene #getter
+    attr_accessor :bank_account 
 
     def initialize(name)
         @name = name #setter
-        @bank_account = 25
-        @happiness = 8
-        @hygiene = 8
+        @bank_account = 25 #constant
+        @happiness = 8 #constant
+        @hygiene = 8 #constant
     end
 
     def happiness=(level)
         @happiness = level
         @happiness = 10 if @happiness > 10
-        @happiness = 0 if @happiness < 0      
+        @happiness = 0 if @happiness < 0 
+        @happiness = happy? if level > 7
     end
 
     def hygiene=(level)
@@ -22,6 +24,10 @@ class Person
         @hygiene = 10 if @hygiene > 10
         @hygiene = 0 if @hygiene < 0      
     end
+
+    # def bank_account
+    #     @bank_account = 25
+    # end
 
 
 
